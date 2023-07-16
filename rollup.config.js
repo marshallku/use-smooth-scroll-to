0519c1eeda438368f5506f08295e018df99cb537
Marshall Ku <marshall@kakao.com>
2023-07-16 22:31:49 +0900

    Add rollup for compiling

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import { babel } from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+import pkg from "./package.json" assert { type: "json" };
+
+const makeExternalPredicate = (externalArr) => {
+    if (externalArr.length === 0) {
+        return () => false;
+    }
+    const pattern = new RegExp(`^(${externalArr.join("|")})($|/)`);
+    return (id) => pattern.test(id);
+};
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [{ file: pkg.module, format: "es" }],
+        external: makeExternalPredicate([...Object.keys(pkg.peerDependencies)]),
+        plugins: [
+            babel({
+                babelHelpers: "bundled",
+            }),
+            peerDepsExternal(),
+            typescript(),
+        ],
+    },
+    {
+        input: "src/index.ts",
+        output: {
+            file: pkg.types,
+            name: "PostScribe",
+        },
+        plugins: [dts()],
+    },
+];
